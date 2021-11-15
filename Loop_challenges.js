81c6ba69829d82ae16fe5612e3e4c31d84bed9b0
Kevin <kevin.ck.nguyen@gmail.com>
2021-11-14 19:28:37 -0800

    First

---
+++ Loop_challenges.js
@@ -0,0 +1,50 @@
+/* Problem 1 */
+console.log ("Problem1")
+for (var i=1; i<20; i+=2) {
+    console.log(i);
+}
+
+/* Problem 2 */
+
+console.log ("Problem2")
+for (var u=100; u>0; u--) {
+    if (u % 3 ==0) {
+        console.log (u);
+    }
+}
+
+
+/* Problem 3 */
+console.log ("Problem3");
+
+arr([4,2.5,1,-0.5,-2,-3.5]);
+
+function arr(arr) {
+    for (var o=0; o<arr.length; o++) {
+        console.log (arr[o]);
+    }
+}
+
+
+/* Problem 4 */
+console.log ("Problem4");
+
+var sum=0
+
+for (var p=0; p<=100; p++) {
+    sum += p;
+}
+
+console.log (sum)
+
+/* Problem 5 */
+
+console.log ("Problem5");
+
+var product=1
+
+for (var t=1; t<=12; t++) {
+    product *= t;
+}
+
+console.log (product);
